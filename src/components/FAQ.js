6548139c7c271da26a5404683c5aa63adeec4072
shzn-mhd <mohamadshazan221@gmail.com>
2025-01-04 19:48:01 +0530

    updated FAQ

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -7,7 +7,7 @@ const FAQ = () => {
       <ul>
         <li><strong>How is the weather data sourced?</strong> We use OpenWeather API for reliable forecasts.</li>
         <li><strong>Is this app free?</strong> Yes, it is completely free for farmers.</li>
-        <li><strong>Can I use it for locations other than Colombo?</strong> Future updates will include location-based weather.</li>
+        <li><strong>Can I use it for locations other than Badulla?</strong> Future updates will include location-based weather.</li>
       </ul>
     </div>
   );
